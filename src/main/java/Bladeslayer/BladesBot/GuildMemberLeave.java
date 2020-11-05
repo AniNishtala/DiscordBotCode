ad18518a97753a93c7fa5c9de98e75946bfdc93d
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2020-11-04 20:38:04 -0600

    Update GuildMemberLeave.java

--- src/main/java/Bladeslayer/BladesBot/GuildMemberLeave.java
+++ src/main/java/Bladeslayer/BladesBot/GuildMemberLeave.java
@@ -1,10 +1,7 @@
 package Bladeslayer.BladesBot;
-
-
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
 import java.awt.*;
 import java.util.Random;
 
@@ -17,8 +14,9 @@ public class GuildMemberLeave extends ListenerAdapter {
         int num = rand.nextInt(messages.length);
         EmbedBuilder join = new EmbedBuilder();
         join.setColor(Color.MAGENTA);
-         join.setDescription(messages[num].replace("[member]" , event.getUser().getAsTag()));
-        event.getGuild().getTextChannelsByName("general",true ).get(0).sendMessage(join.build()).queue();
+
+        join.setDescription(messages[num].replace("[member]" , event.getUser().getAsTag()));
+        event.getGuild().getTextChannelsByName("welcome",true ).get(0).sendMessage(join.build()).queue();
         event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRolesByName("Member", true).get(0)).complete();
     }
 }
