4514d680908040d81d0d09c855ae63dc57e1e6e4
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2020-11-04 20:39:02 -0600

    Update GuildMessageReceived.java

--- src/main/java/Bladeslayer/BladesBot/GuildMessageReceived.java
+++ src/main/java/Bladeslayer/BladesBot/GuildMessageReceived.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class GuildMessageReceived extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        //To add X reaction to every server remove if(event.getGuild().getId().equals("766360284754870363))
         if (event.getGuild().getId().equals("766360284754870363")) {
             event.getMessage().addReaction("❌").queue();
         } else {
